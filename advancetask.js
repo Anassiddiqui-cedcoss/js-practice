d3d32293f3c22d2e78d89974b4ade4ae364a8019
Anassiddiqui-cedcoss <mohdanassiddiqui@cedcoss.com>
2022-08-02 15:49:30 +0530

    Write a debug function and execute it whenever salary becomes less than or equals to zero.

--- advancetask.js
+++ advancetask.js
@@ -1,58 +1,70 @@
+
 const arr=[]
 function populate(){
     text=""
     for(i=0; i<arr.length;i++)
     {
       
-    text+=
+        text+=
     "<tr><td>" + arr[i].id +"</td><td>"
     + arr[i].Name +"</td><td>"
     + arr[i].salary +"</td><td> <a onclick='del(this)'>Delete</a></td></tr>"
       
-      
-     }
+    
+}
      document.getElementById('result').innerHTML=text;
      
-   }
-function myresult()
-{
-arr.push(
-
-
-    {
-        id:document.getElementById("id").value,
-        Name:document.getElementById("name").value,
-        salary:document.getElementById("salary").value,
-        action:""
-        
     }
-)
-
-for(i=0; i<arr.length; i++){
-
-  
-
-    text=
-    "<tr><td>" + arr[i].id +"</td><td>"
-    + arr[i].Name +"</td><td>"
-    + arr[i].salary +"</td><td> <a  onclick='del(this)' style='text-decoration:underline;'>Delete</a></td></tr>"
-}
-    document.getElementById("result").innerHTML=document.getElementById("result").innerHTML+text;
-
-}
+    function myresult()
+    {
+        arr.push(
+            
+            
+            {
+                id:document.getElementById("id").value,
+                Name:document.getElementById("name").value,
+                salary:document.getElementById("salary").value,
+                action:""
+                
+            }
+            )
+            
+            for(i=0; i<arr.length; i++){
+                
+                
+                
+                text=
+                "<tr><td>" + arr[i].id +"</td><td>"
+                + arr[i].Name +"</td><td>"
+                + arr[i].salary +"</td><td> <a  onclick='del(this)' style='text-decoration:underline;'>Delete</a></td></tr>"
+            }
+            if(salary.value<=0){
+                debugger;
+                // alert();
+            }
+            
+            // try{
+            //     if(id=="" && Name=="" && salary==""){
+            //         throw "It should be filled "
+            //     }
+            //     if(id)
+            // }
+            document.getElementById("result").innerHTML=document.getElementById("result").innerHTML+text;
+            
+        }
 function del(args){
-   var tr=args.parentNode.parentNode;
-   index=tr.rowIndex;
-   arr.splice(index,1);
-  populate();
+    var tr=args.parentNode.parentNode;
+    index=tr.rowIndex;
+    arr.splice(index,1);
+    populate();
     
-  
+    
 }
 //##############################################Task-02#######################################################
 class details{constructor(id,Name,salary)
-{
+    {
         
-    this.id = id;
+        this.id = id;
     this.Name=Name;
     this.salary=salary;
 } 
@@ -79,18 +91,18 @@ obj.employees[0].Id + " " + obj.employees[0].Name+" "+obj.employees[0].Totalsala
 const data=[];
 class data1{constructor(id,Name,salary)
     {
-            
+        
         this.id = id;
         this.Name=Name;
         this.salary=salary;
     } 
     JsonAccept(jsonobj){
     var obj1 = JSON.parse(jsonobj);
-        
+    
     this.id = obj1.id;
     this.Name=obj1.Name;
     this.salary=obj1.salary;
-    }
+}
 }
 let objdetails = new data1(1,"john",8000);
 t1='{"id":"1","Name":"John","salary":"8000" }';
@@ -109,18 +121,18 @@ const array=[
     {id:321,name:"Abhinav",salary:5000,updatedsalary:""}
 ];
 function populatearr(){
-   
     
-     try {
+    
+    try {
      for(i=0;i<array.length;i++)
      {
          
-        updatedsalary=array[i].salary*1.10;
-        array[i].updatedsalary=updatedsalary;
+         updatedsalary=array[i].salary*1.10;
+         array[i].updatedsalary=updatedsalary;
          
         }
-    
-     
+        
+        
     }
     catch(err) {
         alert();
@@ -130,7 +142,7 @@ function populatearr(){
     table="<table><tr><th>ID</th><th>Name</th><th>TotalSalary</th><th>updatedsalary</th></tr>";
     for(i=0; i<array.length;i++)
     {
-
+        
     table+=
     "<tr><td>" + array[i].id +"</td><td>"
     + array[i].name +"</td><td>"
@@ -138,12 +150,12 @@ function populatearr(){
     table+="</table>";
       
       
-     }
-     document.getElementById('table3').innerHTML=table;
-     
-   }
+}
+document.getElementById('table3').innerHTML=table;
 
-   populatearr();
+}
+
+populatearr();
 
 //##############################################Task-06######################################################
 
@@ -157,21 +169,23 @@ function populatearr1(){
     table1="<table><tr><th>ID</th><th>Name</th><th>TotalSalary</th><th>salary(15%)</th></tr>";
     for(i=0; i<array1.length;i++)
     {
-
-    table1+=
+        
+        table1+=
     "<tr><td>" + array1[i].id +"</td><td>"
     + array1[i].name +"</td><td>"
     + array1[i].salary +"</td><td>"+array1[i].output+"</td></tr><br>";
     table1+="</table>";
 }
-      
+
       
 document.getElementById('table4').innerHTML=table1;
-     }
+}
      updatesalary();
      function updatesalary(){
+         
+         array1.map((x)=>x.output=x.salary*0.15);
+         populatearr1();
+        }
+//#######################################Task-07#############################################################
+//##########################################Task-08############################################################
 
-          array1.map((x)=>x.output=x.salary*0.15);
-          populatearr1();
-     }
-//######################################Task-07#############################################################
