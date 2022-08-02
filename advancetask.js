cf8299a805278bac73d813419340d6c546403419
Anassiddiqui-cedcoss <mohdanassiddiqui@cedcoss.com>
2022-08-02 10:55:44 +0530

    Use 'this' keyword to display the employee details (stored in JSON array) on the HTML page.

--- advancetask.js
+++ advancetask.js
@@ -63,7 +63,7 @@ text+="<tr><td>"+alldetails.id+"</td><td>"+alldetails.Name+"</td><td>"+alldetail
 text+="</table>";
 document.getElementById("table1").innerHTML=text;
 
-//###############################################task=04######################################################
+//###############################################task=03########################################################################
 let txt = '{"employees":[' +
 '{"Id":"123","Name":"Anas","Totalsalary":"10000" },' +
 '{"Id":"456","Name":"Akash","Totalsalary":"20000"},' +
@@ -75,3 +75,27 @@ const obj = JSON.parse(txt);
 document.getElementById("table2").innerHTML =
 obj.employees[0].Id + " " + obj.employees[0].Name+" "+obj.employees[0].Totalsalary;
 
+//###############################################Task-04##########################################################################
+const data=[];
+class data1{constructor(id,Name,salary)
+    {
+            
+        this.id = id;
+        this.Name=Name;
+        this.salary=salary;
+    } 
+    JsonAccept(jsonobj){
+    var obj1 = JSON.parse(jsonobj);
+        
+    this.id = obj1.id;
+    this.Name=obj1.Name;
+    this.salary=obj1.salary;
+    }
+}
+let objdetails = new data1(1,"john",8000);
+t1='{"id":"1","Name":"John","salary":"8000" }';
+objdetails.JsonAccept(t1);
+var text="<table><tr><th>ID</th><th>Name</th><th>TotalSalary</th></tr>";
+text+="<tr><td>"+objdetails.id+"</td><td>"+objdetails.Name+"</td><td>"+objdetails.salary+"</td></tr>";
+text+="</table>";
+document.getElementById("tabledetails").innerHTML=text;
